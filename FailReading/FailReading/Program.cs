6bbda9df24273b93e79dc99b247d9c8e4838676d
DESKTOP-NKURHCG\user <shunshun6640@outlook.jp>
2025-08-04 09:26:54 +0900

    単純な計算

---
+++ FailReading/FailReading/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+namespace FailReading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int a = 2;
+            int b = 3;
+            int c = 4;
+            int x,y,z;
+
+            x = a + b;
+            y = c - a;
+            z = b * c;
+            Console.WriteLine("x={0} v={1} z={2}", x, y, z);
+
+        }
+    }
+}
